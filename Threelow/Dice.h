ed5b8fd58683e203796936add43ba762f82bc390
Nicholas Fung <nbfung@sfu.ca>
2017-10-04 14:35:38 -0700

    Add Dice class

---
+++ Threelow/Dice.h
@@ -0,0 +1,13 @@
+//
+//  Dice.h
+//  Threelow
+//
+//  Created by Nicholas Fung on 2017-10-04.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dice : NSObject
+
+@end
