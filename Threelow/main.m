a564773b420aeab1d512adf3eeaf38fe0feecfeb
Nicholas Fung <nbfung@sfu.ca>
2017-10-04 14:22:50 -0700

    Initial Commit

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Nicholas Fung on 2017-10-04.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
